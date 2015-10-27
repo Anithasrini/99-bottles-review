e59a23df229a8caebdc88c2c24938d3a9f0923b2
Anithasrini <sanitha32@gmail.com>
2015-10-26 17:54:48 -0700

    Update script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,23 @@
-var bottleLyrics = function(amount) {
+var bottleLyrics = function() {
 
-  if(amount === 0) {
+  if(i === 0) {
      return "no more bottles of beer on the wall, no more bottles of beer.Go to store and buy some more, 99 bottles of beer on the wall";
   }
 
-  if(amount === 1) {
+  if(i === 1) {
      return  "1 bottle of beer on the wall, 1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.";
   }
-  if(amount === 2) {
+  if(i === 2) {
     return "2 bottles of beer on the wall, 2 bottles of beer.Take one down and pass it around, 1 bottle of beer on the wall.";
   }
 
-var amount = 99; amount > 0; amount -1
-  if(amount > 0) {
-      return "(amount) bottles of beer on the wall, (amount) bottles of beer.Take one down and pass it around, (amount-1) more bottles of beer on the wall.";
+ var i = 99; i > 0; i -1 {
+ if(i > 0) {
+      return "(i) bottles of beer on the wall, (i) bottles of beer.Take one down and pass it around, (i-1) more bottles of beer on the wall.";
+  }
   }
   else {
  return false;
  }
-
-  $(".song").append(result);
-  $("#result").show();
 
 }
