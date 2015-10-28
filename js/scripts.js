15a1aca5e915fd1142324b0ec663a7e399e4b495
Anithasrini <sanitha32@gmail.com>
2015-10-28 16:16:19 -0700

    Update script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,17 @@
-var bottleLyrics = function() {
-
-  if(i === 0) {
-     return "no more bottles of beer on the wall, no more bottles of beer.Go to store and buy some more, 99 bottles of beer on the wall";
-  }
-
-  if(i === 1) {
-     return  "1 bottle of beer on the wall, 1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.";
+var bottleLyrics = function(i) {
+  for var (i = 99; i > 0; i -1) {
+   if(i > 2) {
+      return ((i) + "bottles of beer on the wall," + (i) + "bottles of beer.Take one down and pass it around," + (i-1) + "more bottles of beer on the wall.");
   }
   if(i === 2) {
-    return "2 bottles of beer on the wall, 2 bottles of beer.Take one down and pass it around, 1 bottle of beer on the wall.";
+    return ("2 bottles of beer on the wall, 2 bottles of beer.Take one down and pass it around, 1 bottle of beer on the wall.");
   }
-
- var i = 99; i > 0; i -1 {
- if(i > 0) {
-      return "(i) bottles of beer on the wall, (i) bottles of beer.Take one down and pass it around, (i-1) more bottles of beer on the wall.";
+  if(i === 1) {
+     return  ("1 bottle of beer on the wall, 1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.");
   }
+  if(i === 0) {
+     return ("no more bottles of beer on the wall, no more bottles of beer.Go to store and buy some more, 99 bottles of beer on the wall");
   }
-  else {
- return false;
  }
-
+  
 }
