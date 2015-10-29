a1483164df52be8346fcba42fb8db23c4a833236
Anithasrini <sanitha32@gmail.com>
2015-10-28 19:38:52 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
-var bottleLyrics = function(i) {
-  for var (i = 99; i > 0; i -1) {
-   if(i > 2) {
-      return ((i) + "bottles of beer on the wall," + (i) + "bottles of beer.Take one down and pass it around," + (i-1) + "more bottles of beer on the wall.");
-  }
-  if(i === 2) {
-    return ((i)  + "bottles of beer on the wall," (i) + "bottles of beer.Take one down and pass it around," +  (i) + "bottle of beer on the wall.");
-  }
-  if(i === 1) {
-     return  ((i) + "bottle of beer on the wall," + (i) + "bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.");
-  }
-  if(i === 0) {
-     return ("no more bottles of beer on the wall, no more bottles of beer.Go to store and buy some more, 99 bottles of beer on the wall");
-  }
- }
-  
-}
+
+var bottleLyrics = function(input) {
+  var result = "";
+  for (var i=input; i >= 0; i--) {
+
+    if(i > 2) {
+      return ((i) + " bottles of beer on the wall, " + (i) + " bottles of beer.Take one down and pass it around, " + (i-1) + " bottles of beer on the wall.");
+    }
+    if(i === 2) {
+      return ((i)  + " bottles of beer on the wall, " + (i) + " bottles of beer.Take one down and pass it around, " +  (i-1) + " bottle of beer on the wall.");
+    }
+    if(i === 1) {
+      return  ((i) + " bottle of beer on the wall, " + (i) + " bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.");
+    }
+    if(i === 0) {
+      return ("no more bottles of beer on the wall, no more bottles of beer.Go to store and buy some more, 99 bottles of beer on the wall");
+    }
+  };
+};
+
+
